390b9bd0dc39818eede6c91c053acccd749b79e5
Peter <73311957+peter-korkinov@users.noreply.github.com>
2022-01-04 21:47:57 +0200

    third exercise first task

--- 03. Static and Class methods/01. Photo Album.py
+++ 03. Static and Class methods/01. Photo Album.py
@@ -31,16 +31,3 @@ class PhotoAlbum:
             output.append(divisor)
 
         return '\n'.join(output)
-
-
-album = PhotoAlbum(2)
-
-print(album.add_photo("baby"))
-print(album.add_photo("first grade"))
-print(album.add_photo("eight grade"))
-print(album.add_photo("party with friends"))
-print(album.photos)
-print(album.add_photo("prom"))
-print(album.add_photo("wedding"))
-
-print(album.display())
