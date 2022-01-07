551880105cfcd73fd0bce6465c83a1752554739f
Peter <73311957+peter-korkinov@users.noreply.github.com>
2022-01-07 21:25:20 +0200

    cheetah class

--- 04. Encapsulation/01. Wild Cat Zoo/cheetah.py
+++ 04. Encapsulation/01. Wild Cat Zoo/cheetah.py
@@ -2,5 +2,5 @@ from .animal import Animal
 
 
 class Cheetah(Animal):
-    def __init__(self):
-        pass
+    def __init__(self, name, gender, age):
+        super().__init__(name, gender, age, money_for_care=60)
